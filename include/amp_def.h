62c7c3333f030948d4784bd3adb5242a9fa8c44d
nevare <zeroender@gmail.com>
2026-04-12 14:01:45 -0500

    8.1 Random number generator

--- include/amp_def.h
+++ include/amp_def.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <cstdint>
+#include <cstdlib>
 #include <limits>
 #include <memory>
+#include <random>
 #include <sys/types.h>
 
 using u8  = uint8_t;
@@ -63,4 +65,18 @@ inline f64
 degrees_to_radians(f64 degrees)
 {
     return degrees * pi/180.0;
+}
+
+inline f64
+random_f64()
+{
+    static std::uniform_real_distribution<f64> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline f64
+random_f64(f64 min, f64 max)
+{
+    return min + (max-min)*random_f64();
 }
