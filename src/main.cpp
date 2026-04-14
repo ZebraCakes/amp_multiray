5623904672e99c2a17a8b4593611d7496d5e3117
nevare <zeroender@gmail.com>
2026-04-13 22:01:44 -0500

    11.5 Modeling a hollow glass sphere

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,14 @@ main(int arg_count, char** args)
 
     shared_ptr<material> material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     shared_ptr<material> material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    shared_ptr<material> material_left   = make_shared<dielectric>(1.00/1.33);
+    shared_ptr<material> material_left   = make_shared<dielectric>(1.50);
+    shared_ptr<material> material_bubble = make_shared<dielectric>(1.00/1.50);
     shared_ptr<material> material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
     world.objects.push_back(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.objects.push_back(make_shared<sphere>(point3( 0.0,    0.0, -1.2),   0.5, material_center));
     world.objects.push_back(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+    world.objects.push_back(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.4, material_bubble));
     world.objects.push_back(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
     camera cam;
